2bbf0984cee26f3d0785e964f69e78ec4edb1c6a
cricripaz6 <cricripaz6@gmail.com>
2019-04-25 08:04:13 -0400

    Create login Activity

---
+++ app/src/main/java/com/android_q_a_q_a/proyecto/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.android_q_a_q_a.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
---
+++ app/src/main/java/com/android_q_a_q_a/proyecto/Menu_AsGuest_Activity.java
@@ -0,0 +1,13 @@
+package com.android_q_a_q_a.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu_AsGuest_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__as_guest_);
+    }
+}
