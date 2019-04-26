33b929f5b22e7c489928fc853d8bda0119372903
cricripaz6 <cricripaz6@gmail.com>
2019-04-26 09:08:31 -0400

    MainActivity and MenuActivity modified. Video and images added.

--- app/src/main/java/com/android_q_a_q_a/proyecto/MainActivity.java
+++ app/src/main/java/com/android_q_a_q_a/proyecto/MainActivity.java
@@ -3,11 +3,16 @@ package com.android_q_a_q_a.proyecto;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.SharedElementCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.VideoView;
+
 
 public class MainActivity extends AppCompatActivity {
     // Create a VideoView variable, a MediaPlayer variable, and an int to hold the current
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     int mCurrentVideoPosition;
     Button Login,LoginAsGuest ;
     Intent LoginIntent,LoginAsGuestIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,13 +109,6 @@ public class MainActivity extends AppCompatActivity {
         mMediaPlayer.release();
         mMediaPlayer = null;
     }
-    /*================================ Important Section! ================================
-    We must override onPause(), onResume(), and onDestroy() to properly handle our
-    VideoView.
-     */
-/*
 
-
-
-*/
+    
 }
--- app/src/main/java/com/android_q_a_q_a/proyecto/MenuActivity.java
+++ app/src/main/java/com/android_q_a_q_a/proyecto/MenuActivity.java
@@ -10,4 +10,6 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+
 }
