05b371257150f969b5e8dfe56b548b96e59a8980
slaguna17 <slaguna17@gmail.com>
2019-05-08 20:59:07 -0400

    implementacion firebase, proceso de notas

--- app/src/main/java/com/android_q_a_q_a/proyecto/GradeActivity.java
+++ app/src/main/java/com/android_q_a_q_a/proyecto/GradeActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import android.widget.ArrayAdapter;
@@ -15,27 +16,21 @@ public class GradeActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-
-
-    Firebase nombrelol;
-    ArrayList<String> listViewGrades = new ArrayList<>();
+    ArrayList<String> arrayListViewGrades = new ArrayList<>();
     ListView listViewGrades;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade);
         listViewGrades= findViewById(R.id.listViewGrades);
-        Firebase.setAndroidContext(this);
-        nombrelol = new Firebase (https://www.youtube.com/watch?v=cyzZNRUK_ik);
         ArrayAdapter<String> dataArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
-        nombrelol.addChildEventListener(new ChildEventListener))
 
     }
 
-    public void escribirNotas(View view){
-        EditText nota = findViewById();
-        EditText estudiante = findViewById();
-        DatabaseReference TeacherRef = database.getReference("notas/"+estudiante);
-        TeacherRef.setValue(nota);
+    public void escribirNotas(android.view.View view){
+        //EditText nota = findViewById();
+        //EditText estudiante = findViewById();
+       // DatabaseReference TeacherRef = database.getReference("notas/"+estudiante);
+        //TeacherRef.setValue(nota);
     }
 }
