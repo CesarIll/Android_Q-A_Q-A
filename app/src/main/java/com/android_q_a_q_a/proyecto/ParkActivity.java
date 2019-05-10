d6cfbce71f9fdeebb1906dce40602a81f2b67caa
Fernanda <f.l.m.m.0027@gmail.com>
2019-05-10 08:40:48 -0400

    this shouldnt stop 3

--- app/src/main/java/com/android_q_a_q_a/proyecto/ParkActivity.java
+++ app/src/main/java/com/android_q_a_q_a/proyecto/ParkActivity.java
@@ -13,37 +13,11 @@ import android.widget.Toast;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
-public class ParkActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
-    ImageView mapa;
-        @Override
+public class ParkActivity extends AppCompatActivity{
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park);
-        mapa = findViewById(R.id.mapa);
-        mapa.setImageResource(R.drawable.upblogo);
     }
 
-    public void mostrarPopup (android.view.View v){
-        PopupMenu popup = new PopupMenu(this , v);
-        popup.setOnMenuItemClickListener(this);
-        popup.inflate(R.menu.popupmenu);
-        popup.show();
-    }
-
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item1:
-               mapa.setImageResource(R.drawable.mapacampus);
-                return true;
-            case R.id.item2:
-                mapa.setImageResource(R.drawable.mapacbba);
-                return true;
-            case R.id.item3:
-                Toast.makeText(this, "¡Estamos trabajando en el diseño de Posgrado-La Paz, ¡gracias por tu paciencia!", Toast.LENGTH_LONG).show();
-                return true;
-            default:
-                return false;
-        }
-    }
 }
