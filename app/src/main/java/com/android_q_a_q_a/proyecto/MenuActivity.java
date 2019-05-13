934a544f9bedae8b249633ce63e295ed7d80af43
slaguna17 <slaguna17@gmail.com>
2019-05-12 22:44:44 -0400

    layout de la puerta y la puerta terminada

--- app/src/main/java/com/android_q_a_q_a/proyecto/MenuActivity.java
+++ app/src/main/java/com/android_q_a_q_a/proyecto/MenuActivity.java
@@ -1,30 +1,51 @@
 package com.android_q_a_q_a.proyecto;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
 
 public class MenuActivity extends AppCompatActivity {
 
     Intent intent;
     GridLayout menuGrid;
+    ImageButton logout;
+    SharedPreferences sharedPreferences;
+    CheckBox checkBox;
+    Intent mainIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        sharedPreferences = getSharedPreferences("DatoCheckbox", MODE_PRIVATE);
+
+        logout= (ImageButton) findViewById(R.id.logoutbutton);
+        logout.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("checkBoxValue", false);
+                editor.apply();
+                editor.commit();
+                mainIntent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+            }
+        });
         //--------------------------------------------------------------
         //Menu OnClick in CardViews
 
         menuGrid = (GridLayout) findViewById(R.id.menuGrid);
         setSingleEvent(menuGrid);
 
-
     }
+
     private void setSingleEvent(GridLayout mainGrid) {
         //Loop all child item of Main Grid
         for (int i = 0; i < mainGrid.getChildCount(); i++) {
@@ -59,5 +80,6 @@ public class MenuActivity extends AppCompatActivity {
             });
         }
     }
+
 
 }
