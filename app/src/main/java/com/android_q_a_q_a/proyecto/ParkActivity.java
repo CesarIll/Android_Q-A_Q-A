a2e5de5c9d97c41471e0450f9245824311cb231e
Fernanda <f.l.m.m.0027@gmail.com>
2019-05-13 01:21:12 -0400

    park failure

--- app/src/main/java/com/android_q_a_q_a/proyecto/ParkActivity.java
+++ app/src/main/java/com/android_q_a_q_a/proyecto/ParkActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
 
 import uk.co.senab.photoview.PhotoViewAttacher;
 
-public class ParkActivity extends AppCompatActivity{
+public class ParkActivity extends AppCompatActivity implements View.OnClickListener{
     Button buttonone = findViewById(R.id.parkOne);
     Button buttontwo = findViewById(R.id.parkTwo);
     Button buttonthree = findViewById(R.id.parkThree);
@@ -27,9 +27,39 @@ public class ParkActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park);
-    }
-    public void parking (View view){
 
+        buttonone.setOnClickListener(this);
+        buttontwo.setOnClickListener(this);
+        buttonthree.setOnClickListener(this);
+        buttonfour.setOnClickListener(this);
+        buttonfive.setOnClickListener(this);
+        buttonsix.setOnClickListener(this);
+        buttonseven.setOnClickListener(this);
+    }
+@Override
+    public void onClick (View view){
+        switch (view.getId()){
+            case R.id.parkOne:
+              CharSequence cs =  buttonone.getText();
+              Integer number1 = Integer.parseInt(cs.toString());
+              if (number1 <0){
+                  number1--;
+                  Toast.makeText(this, "Espacio ocupado", Toast.LENGTH_SHORT).show();
+                  if (number1==0){
+                      Toast.makeText(this, "¡Ocupaste el último espacio!", Toast.LENGTH_SHORT).show();
+                      buttonone.setBackgroundResource(R.drawable.fullpark);
+                  }
+                  buttonone.setEnabled(false);
+                  buttontwo.setEnabled(false);
+                  buttonthree.setEnabled(false);
+                  buttonfour.setEnabled(false);
+                  buttonfive.setEnabled(false);
+                  buttonsix.setEnabled(false);
+                  buttonseven.setEnabled(false);
+              } else {
+                  Toast.makeText(this, "Todos los parqueos aquí están ocupados", Toast.LENGTH_SHORT).show();
+              }
+              break;
         }
 
     }
